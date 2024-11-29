a235bed43f6c7d2ef8afe144b63d24ee0c627a50
Mykhailo Litvinov <mlitvino@student.hive.fi>
2024-11-29 10:15:05 +0200

    new implementation

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,103 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:35:25 by mlitvino          #+#    #+#             */
-/*   Updated: 2024/11/27 13:43:11 by mlitvino         ###   ########.fr       */
+/*   Updated: 2024/11/29 10:13:55 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_substr(char *buffer, int i)
+char	*ft_substr()
 {
-	char	*new_line;
-
-	if (buffer[i] == '\n')
-		i++;
-	new_line = (char *)malloc(sizeof(char) * i + 1);
-	if (new_line == NULL)
-		return (NULL);
-	new_line[i] = '\0';
-	while (--i >= 0)
-	{
-		new_line[i] = buffer[i];
-	}
-	return (new_line);
+	return (NULL);
 }
 
-char	*ft_realloc(char *buffer, int *buf_size, int buf_len)
+char	*ft_strjoin()
 {
-	char	*new_buffer;
-	int		i;
-
-	i = 0;
-	new_buffer = (char *)malloc(sizeof(char) * (*buf_size * 2) + 1);
-	if (new_buffer == NULL)
-		return (free(buffer), NULL);
-	while (i < buf_len)
-	{
-		new_buffer[i] = buffer[i];
-		i++;
-	}
-	*buf_size *= 2;
-	free(buffer);
-	return (new_buffer);
+	return (NULL);
 }
 
-char	*ft_find(char *buffer, int fd)
+char	*ft_realloc()
 {
-	int		i;
-	int		buf_size;
-	char	*new_line;
+	return (NULL);
+}
 
-	buf_size = BUFFER_SIZE;
-	i = 0;
-	while (i < buf_size)
+int	ft_is_line()
+{
+	return (0);
+}
+
+int	ft_mark()
+{
+	return (0);
+}
+
+int	ft_is_null(char *short_buff, char *long_buff, int fd)
+{
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (1);
+	if (long_buff == NULL)
+		long_buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (long_buff == NULL)
+		return (1);
+	short_buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE) + 1);
+	if (short_buff == NULL)
+		return (free(long_buff), 1);
+	return (0);
+}
+
+int	ft_read(char *long_buff, char *short_buff, int fd)
+{
+	int	len;
+
+	len = read(fd, short_buff, BUFFER_SIZE);
+	if (len < 0)
+		return (free(short_buff), free(long_buff), 0);
+	if (len == 0)
 	{
-		if (read(fd, &buffer[i], 1) <= 0)
-			return (free(buffer), NULL);
-		if (buffer[i] == 0 || buffer[i] == '\n')
+		if (ft_strlen(long_buff) == 0)
 		{
-			new_line = ft_substr(buffer, i);
-			return (free(buffer), new_line);
+			free(long_buff);
+			long_buff = NULL;
+			return (free(short_buff), 0);
 		}
-		i++;
-		if (i == buf_size)
-		{
-			buffer = ft_realloc(buffer, &buf_size, i);
-			if (buffer == NULL)
-				return (NULL);
-		}
+		else
+			return (free(short_buff), 0);
 	}
-	return (free(buffer), NULL);
+	return (1);
 }
 
 char	*get_next_line(int fd)
 {
-	static char		*buffer;
+	static char		*long_buff;
+	char			*short_buff;
+	char			*new_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (ft_is_null(short_buff, long_buff, fd))
 		return (NULL);
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (buffer == NULL)
-		return (NULL);
-	return (ft_find(buffer, fd));
+	while (1)
+	{
+		if (ft_read(long_buff, short_buff, fd) == 0)
+			return (NULL);
+		if (ft_check("\n\0") == 0)
+		{
+			if (f_realloc(short_buff) == NULL)
+				return (free(short_buff), free(long_buff), NULL);
+			continue;
+		}
+		if (mark("\nfd") == 0)
+		{
+			if (ft_substr() != NULL)
+				ft_mark();
+			else
+				return (NULL);
+			return (ft_substr());
+		}
+		new_line = ft_strjoin(ft_substr, ft_substr);
+		if (new_line == NULL)
+			return (free(short_buff), free(long_buff), NULL);
+		return (new_line);
+	}
+	return (new_line);
 }
+
