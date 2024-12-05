6ec3167d10569d7cbbae90fcb722bb83e23c914f
mlitvino <106635805+RabbitFrank@users.noreply.github.com>
2024-12-05 10:42:34 +0200

    Add files via upload

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 17:59:36 by mlitvino          #+#    #+#             */
+/*   Updated: 2024/12/04 14:31:37 by mlitvino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+void	ft_bzero(char *buffer, int size)
+{
+	if (buffer == NULL)
+		return ;
+	while (size > 0)
+	{
+		buffer[size - 1] = '\0';
+		size--;
+	}
+}
+
+int	ft_strlen(char *buffer)
+{
+	int	len;
+
+	len = 0;
+	if (buffer == NULL)
+		return (0);
+	while (buffer[len])
+		len++;
+	return (len);
+}
+
+char	*ft_substr(char *buffer, int *buf_len)
+{
+	char	*new_line;
+	int		i;
+	int		j;
+
+	i = 0;
+	new_line = (char *)malloc(sizeof(char) * (*buf_len + 2));
+	if (new_line == NULL)
+		return (NULL);
+	while (i <= *buf_len)
+	{
+		// printf("\nchar : %d\n", buffer[i]); // DELETE
+		new_line[i] = buffer[i];
+		i++;
+	}
+	new_line[i] = '\0';
+	j = 0;
+	while (buffer[i])
+		buffer[j++] = buffer[i++];
+	buffer[j] = '\0';
+	return (new_line);
+}
+
+int	ft_strchr(char *buffer, int *buf_len)
+{
+	int	i;
+
+	i = 0;
+	if (!buffer || *buf_len <= 0)
+		return (-1);
+	while (i < *buf_len)
+	{
+		if (buffer[i] == '\n' || buffer[i] == '\0')
+		{
+			*buf_len = i;
+			return (1);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_realloc(char *buffer, int *new_size)
+{
+	char	*new_buffer;
+	int		i;
+	int		buf_size;
+
+	i = 0;
+	buf_size = ft_strlen(buffer);
+	new_buffer = (char *)malloc(sizeof(char) * (buf_size + BUFFER_SIZE + 1));
+	if (new_buffer == NULL)
+		return (NULL);
+	if (new_size != NULL)
+		*new_size = buf_size + BUFFER_SIZE;
+	while (i < buf_size)
+	{
+		new_buffer[i] = buffer[i];
+		i++;
+	}
+	ft_bzero(new_buffer + buf_size, BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		ft_bzero(new_buffer, BUFFER_SIZE);
+	// if (buffer != NULL)
+	// 	free(buffer);
+	return (free(buffer), new_buffer);
+}
